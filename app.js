74170f8259dda1b298214b55b980b0e8079a9c08
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-03-22 00:33:17 +0200

    setting up router

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 require("dotenv");
+const productsRouter = require("./routes/products");
 
 const app = express();
+app.use("/api/v1/products", productsRouter);
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
